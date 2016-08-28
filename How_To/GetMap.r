2ea7c363355e36ca7e6735ce0dc07118af6b5a62
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-08-29 00:45:53 +0530

    Getting Map

--- How_To/GetMap.r
+++ How_To/GetMap.r
@@ -1,1 +1,8 @@
+# Plotting Map using "ggmap"
 
+library(ggmap)
+lat <- c(6.7535159, 35.5087008)
+lon <- c(68.1623859, 97.395561)
+india_map <- get_map(location = c(lon = mean(lon), lat = mean(lat)), zoom = 5, 
+			   maptype = "terrain", source = "google")
+ggmap(india_map)
