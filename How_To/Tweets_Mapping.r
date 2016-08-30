4d6a26c290c7b94dbb9707feb0d3fb8838d77a57
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-08-30 11:42:04 +0530

    Mapping Tweets

--- How_To/Tweets_Mapping.r
+++ How_To/Tweets_Mapping.r
@@ -1,1 +1,9 @@
+# Tweets Mapping
 
+# Converting both columns of my_tweets_df as numeric
+my_tweets_df$longitude = as.numeric(as.character(my_tweets_df$longitude))
+my_tweets_df$latitude = as.numeric(as.character(my_tweets_df$latitude))
+
+# Mapping with my_tweets_df
+maps <- ggmap(india_map)
+map + geom_point(data = my_tweets_df, aes(x = longitude, y = latitude), color = "red", size = 5, alpha = 0.5)
