4640598effd01e9c4b350496d3cab7694e2509fb
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-08-31 20:21:33 +0530

    Accessing Tweets

--- How_To/Accessing_Tweets.r
+++ How_To/Accessing_Tweets.r
@@ -1,4 +1,7 @@
 # Accessing tweets using "twitterR" Package--
 
 # Providing any twitter handle, geo code location, distance and number of samples
+# Accessing 500 tweets within 2000km
 my_tweets <- searchTwitter('@facebook', geocode='20.593684,78.96288,2000km', n = 500)
+
+# Avoid whitespaces between the geocode location
