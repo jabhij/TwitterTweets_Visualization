b9298a8994553df09a20cfce8f3cac2ad7452125
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-09-21 21:06:44 +0530

    Getting Map

--- How_To/GetMap.r
+++ How_To/GetMap.r
@@ -1,6 +1,9 @@
 # Plotting Map using "ggmap"
 
+# Loading Library
 library(ggmap)
+
+# Accessing Longitude and Latitude
 lat <- c(6.7535159, 35.5087008)
 lon <- c(68.1623859, 97.395561)
 india_map <- get_map(location = c(lon = mean(lon), lat = mean(lat)), zoom = 5, 
