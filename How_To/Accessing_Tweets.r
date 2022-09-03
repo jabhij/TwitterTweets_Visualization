d04e79e735bd761e46c39ad38708d1eca41fa126
Abhishek Jaiswal <jaisabhii@gmail.com>
2022-09-03 20:49:21 +0530

    Update Accessing_Tweets.r

--- How_To/Accessing_Tweets.r
+++ How_To/Accessing_Tweets.r
@@ -4,4 +4,7 @@
 # Accessing 500 tweets within 2000km
 my_tweets <- searchTwitter('@facebook', geocode='20.593684,78.96288,2000km', n = 500)
 
+# Extension for the further analysis
+#my_tweets_extension <- searchTwitter('@facebook', geocode='', n = 12000)
+
 # Avoid whitespaces between the geocode location
