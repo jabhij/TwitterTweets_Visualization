685655ad35dcb01b015def54fff6a3c95ad69ba5
Abhishek Jaiswal <jaisabhii@gmail.com>
2022-09-09 15:11:09 -0500

    Update Data_Extraction.r

--- How_To/Data_Extraction.r
+++ How_To/Data_Extraction.r
@@ -1,7 +1,9 @@
 # Data minning
-# Excluding rows from data frame where longitude/latitude=NA & taking only unique columns
 
+# Excluding rows from data frame where longitude/latitude=NA & taking only unique columns
 my_tweets_df <- subset(my_tweets_df, longitude != "NA" )
 my_tweets_df <- my_tweets_df[c(15:16)]
 my_tweets_df <- unique(my_tweets_df)
+
+# viewing all the tweets
 View(my_tweets_df)
