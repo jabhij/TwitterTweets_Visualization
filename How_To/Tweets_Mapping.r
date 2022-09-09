61b0242bc2c9273f7ee526e4454c8fd60f08036a
Abhishek Jaiswal <jaisabhii@gmail.com>
2022-09-09 15:09:55 -0500

    Update Tweets_Mapping.r

--- How_To/Tweets_Mapping.r
+++ How_To/Tweets_Mapping.r
@@ -4,6 +4,6 @@
 my_tweets_df$longitude = as.numeric(as.character(my_tweets_df$longitude))
 my_tweets_df$latitude = as.numeric(as.character(my_tweets_df$latitude))
 
-# Mapping with my_tweets_df
+# Mapping with my_tweets_df with respect to other details
 maps <- ggmap(india_map)
 map + geom_point(data = my_tweets_df, aes(x = longitude, y = latitude), color = "red", size = 5, alpha = 0.5)
