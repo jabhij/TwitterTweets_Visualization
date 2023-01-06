2a14a7517d827bd9fd23a4d8bfe60a4a29f1b79f
Abhishek Jaiswal <jaisabhii@gmail.com>
2023-01-05 23:15:31 -0600

    Update Get_Data.r

--- How_To/Get_Data.r
+++ How_To/Get_Data.r
@@ -1,4 +1,4 @@
-# Getting required Data frame as per our inputs
+# Required Data frame as per our inputs
 
 # Using Row-Bind functionality 
 my_tweets_df <- do.call("rbind", laaply(my_tweets,as.data.frame))
